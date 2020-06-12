db89b2aad3167fdf89cfd157b7246bb38569132e
shina <hemhem417@gmail.com>
2020-06-12 14:09:40 +0900

    s3から画像ダウンロード時、URL誤りになったため、修正

--- client/src/repository/documentsRepository.js
+++ client/src/repository/documentsRepository.js
@@ -2,12 +2,15 @@ import Repository from './Repository'
 
 const resource = '/api/v1/documents'
 
+// AxiosRequestConfig.baseURLはインスタンス精製後、末尾にハイフンをつけるため、
+// 先頭ハイフンなしの定義を作成
+const resource2 = 'api/v1/documents'
+
 export default {
   getDocument(documentId) {
     return Repository.get(`${resource}/${documentId}`)
   },
   getDocumentUrl(documentId) {
-    console.log(Repository)
-    return `${Repository.defaults.baseURL}${resource}?docKey=${documentId}`
+    return `${Repository.defaults.baseURL}${resource2}?docKey=${documentId}`
   }
 }
